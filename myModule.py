44217180c1754a7450ba3de6865f42de59697fd1
jaegangkim <106479529+jaegangkim@users.noreply.github.com>
2022-07-18 09:53:25 +0900

    0715
    0715

---
+++ myModule.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# ### 모듈
+# - 함수,변수,클래스를 모아놓은 파이썬 파일
+
+# In[1]:
+
+
+def add(a,b):
+    return a+b
+
+
+# In[ ]:
+
+
+class Cat:
+    def __init__(self,name):
+        self.name = name
+    def getName(self):
+        return self.name
+    
+
